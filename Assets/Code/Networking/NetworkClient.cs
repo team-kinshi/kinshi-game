6e7aa9121d106fa816484aa58036967d05f3a09a
cainajm <cainajes@gmail.com>
2019-05-20 19:42:32 -0300

    Socket.io server connection created

---
+++ Assets/Code/Networking/NetworkClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+namespace Project.Networking
+{
+    public class NetworkClient : SocketIOComponent
+    {
+        public override void Start()
+        {
+            base.Start();
+            setupEvents();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        private void setupEvents()
+        {
+            On("open", (E) =>   
+            {
+                Debug.Log("Connection made to the server");
+            });
+        }
+    }
+}
